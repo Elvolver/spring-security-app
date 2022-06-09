65ae48c6a840c3fb9d77313e482b4be314f074a5
elvolver <elvolver@mail.ru>
2022-06-09 09:35:30 +0300

    Initial commit

---
+++ src/main/java/ru/volkovd/springSecurityApp/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,35 @@
+package ru.volkovd.springSecurityApp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.volkovd.springSecurityApp.model.Developer;
+
+import java.util.List;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Иван","Иванов"),
+            new Developer(2L, "Сергей", "Сергеев"),
+            new Developer(3L, "Пётр", "Петров")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
